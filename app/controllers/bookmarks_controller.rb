2086198bd235f7ee8cd4a3ae65b7b649ede331e3
Julien de Lambilly <julien.delambilly@gmail.com>
2023-02-24 15:06:59 +0100

    copie faustine

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,30 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create show]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(params_bookmark)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def params_bookmark
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
+  resources :lists, only: %i[index new create show] do
+    # resources :bookmarks, only: %i[new create show]
+    get "lists/:id", to: "bookmarks#new"
+    post "lists/:id/bookmarks", to: "bookmarks#create"
+  end
+  resources :bookmarks, only: %i[destroy]
 
   # Defines the root path route ("/")
   # root "articles#index"
